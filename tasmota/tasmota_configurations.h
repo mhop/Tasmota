51f19b3702c9e66867ff61cb489f5ae0811dbebc
Marc Hoppe <mhop@posteo.de>
2021-12-16 21:42:35 +0100

    script und zähler ein (auch fuer tasmota-lite)

--- tasmota/tasmota_configurations.h
+++ tasmota/tasmota_configurations.h
@@ -654,8 +654,8 @@
 //#undef USE_TIMERS                                // Disable support for up to 16 timers
 //#undef USE_TIMERS_WEB                            // Disable support for timer webpage
 //#undef USE_SUNRISE                               // Disable support for Sunrise and sunset tools
-//#undef USE_RULES                                 // Disable support for rules
-#undef USE_SCRIPT                                  // Add support for script (+17k code)
+#undef USE_RULES                                 // Disable support for rules
+//#undef USE_SCRIPT                                  // Add support for script (+17k code)
 
 // -- Optional modules -------------------------
 #undef ROTARY_V1                                 // Disable support for MI Desk Lamp
